9695eb421dd6240042101a45b876099e82b764f8
SiriusLL <sirius.love.light@gmail.com>
2021-06-04 23:32:59 -0600

    NavBar, app global container nav css

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+const NavBar = () => {
+  return (
+    <div className="nav">
+      <div className="container">
+        <h2>React Blog</h2>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
